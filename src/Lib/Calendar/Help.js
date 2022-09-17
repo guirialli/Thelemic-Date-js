d15a3f1f46599d196243ededfaef47dbe612c7c4
Guilherme Rialli Oliveira <gui.rialli@gmail.com>
2022-09-17 13:19:06 -0300

    help improve

---
+++ src/Lib/Calendar/Help.js
@@ -0,0 +1,11 @@
+function getHelp(){
+    return `Calendar arguments:
+    -n or --noIcon use this if want get without emoji, after her you can use a another argument
+    -d or --day to get actual day
+    -m or --mon get the moon phase and sign
+    -s or --sun you can get sun sign 
+    -y or -year get the year thelemic
+    `
+}
+
+module.exports= {getHelp}
--- src/Lib/index.js
+++ src/Lib/index.js
@@ -70,12 +70,6 @@ function noIcon(args){
 }
 
 function  help() {
-    console.log(`Arguments:
-    -n or --noIcon use this if want get without emoji, after her you can use a another argument
-    -d or --day to get actual day
-    -m or --mon get the moon phase and sign
-    -s or --sun you can get sun sign 
-    -y or -year get the year thelemic
-    `)
+    console.log( require('./Calendar/Help').getHelp())
 }
 module.exports = { tdate }
